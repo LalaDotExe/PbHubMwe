10950bf7798eb9d74ccfe117be7e7ac43ebdb723
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-05-16 16:28:14 +0800

    installing firebase

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +20,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"pbhub-bd1ef","appId":"1:740098056744:web:c9e62dad65bf8200e70b1d","storageBucket":"pbhub-bd1ef.appspot.com","apiKey":"AIzaSyCmKAIzBa_sAMdM6K_Fzxl-Q99QzhZuQdA","authDomain":"pbhub-bd1ef.firebaseapp.com","messagingSenderId":"740098056744","measurementId":"G-80V962YC9P"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage()),
   ],
 });
