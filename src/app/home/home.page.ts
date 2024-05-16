d78f1e9db0b6d800cfb9578a1457a09af3562772
22ftt1499 <22ftt1499@student.pb.edu.bn>
2024-05-16 21:08:13 +0800

    fetch data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,21 +2,80 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
-  IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBadge, IonMenuButton, IonIcon,
-  IonButton, IonItem, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu, IonSearchbar, IonCardTitle, IonCardHeader, IonCardContent, IonCardSubtitle, IonApp} from '@ionic/angular/standalone';
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonBadge,
+  IonMenuButton,
+  IonIcon,
+  IonButton,
+  IonItem,
+  IonFooter,
+  IonLabel,
+  IonCard,
+  IonList,
+  IonRow,
+  IonCol,
+  IonGrid,
+  IonMenu,
+  IonSearchbar,
+  IonCardTitle,
+  IonCardHeader,
+  IonCardContent,
+  IonCardSubtitle,
+  IonApp,
+} from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { homeOutline, heartOutline, cartOutline ,cart,person,logOut} from 'ionicons/icons';
+import {
+  homeOutline,
+  heartOutline,
+  cartOutline,
+  cart,
+  person,
+  logOut,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonApp, IonCardSubtitle, IonCardContent, IonCardHeader, IonCardTitle, IonSearchbar, 
-    IonItem, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
-    IonButtons, IonBadge, IonMenuButton, IonIcon, IonFooter, IonLabel, IonCard, IonList, IonRow, IonCol, IonGrid, IonMenu,IonCardTitle,IonCardHeader,IonCardContent,IonCardSubtitle,IonBadge], 
-  
+  imports: [
+    IonApp,
+    IonCardSubtitle,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonSearchbar,
+    IonItem,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonBadge,
+    IonMenuButton,
+    IonIcon,
+    IonFooter,
+    IonLabel,
+    IonCard,
+    IonList,
+    IonRow,
+    IonCol,
+    IonGrid,
+    IonMenu,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardContent,
+    IonCardSubtitle,
+    IonBadge,
+  ],
 })
 export class HomePage implements OnInit {
   productOfTheDay: any;
@@ -24,17 +83,15 @@ export class HomePage implements OnInit {
   Product_Trending: any;
 
   constructor(private router: Router) {
-   
     addIcons({
       'home-outline': homeOutline,
       'heart-outline': heartOutline,
       'cart-outline': cartOutline,
-      'cart': cart,
-      'person':person,
-      'logout':logOut
+      cart: cart,
+      person: person,
+      logout: logOut,
     });
-  
-  };
+  }
 
   ngOnInit() {
     // You can fetch the product data here from an API or service
@@ -46,8 +103,8 @@ export class HomePage implements OnInit {
     this.router.navigate(['/product-details'], {
       queryParams: {
         product: JSON.stringify(product),
-        category: category
-      }
+        category: category,
+      },
     });
   }
 }
